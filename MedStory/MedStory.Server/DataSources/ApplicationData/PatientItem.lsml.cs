6b709f789658dccf5a435f063c6bc5b988f06536
alex <alex_samarkin@mail.ru>
2015-06-06 16:33:23 +0300

    master1

---
+++ MedStory/MedStory.Server/DataSources/ApplicationData/PatientItem.lsml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class PatientItem
+    {
+        
+        partial void FIO_Compute(ref string result)
+        {
+            // Присвоение результату значения нужного поля
+            result = FirstName + " " + Name.ToUpper()[0] + "." + MidName.ToUpper()[0] + ".";
+
+        }
+
+        partial void Age_Compute(ref int result)
+        {
+            // Присвоение результату значения нужного поля
+            DateTime nowDate = DateTime.Today;
+            int age = nowDate.Year - BirthDate.Year;
+            if (BirthDate > nowDate.AddYears(-age)) age--;
+            result = age;
+
+        }
+    }
+}
